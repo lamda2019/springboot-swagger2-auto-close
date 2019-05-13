836e08d4c174a34ca611a0df33e739d2f552a025
e <569188901@qq.com>
2019-05-13 14:34:53 +0800

    禁用Swagger2

---
+++ src/main/java/com/wsc/SpringbootSwagger2AutoCloseApplication.java
@@ -0,0 +1,13 @@
+package com.wsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSwagger2AutoCloseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSwagger2AutoCloseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wsc/SpringbootSwagger2AutoCloseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wsc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootSwagger2AutoCloseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
